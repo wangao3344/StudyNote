37e85607852a45c2610c09867f7acbaadbb59d0d
shane <wangao3344@163.com>
2018-08-15 10:31:23 +0800

    Mrwang进行了变量的修改

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello");
+		//Mrwang进行变量的添加
+		int b =6;
 	}
 }
--- World.kt
+++ World.kt
@@ -1,3 +1,5 @@
 fun main(al:ArrayList<String>){
 println("world")
+//Mrwang进行变量的添加
+var c =3
 }
